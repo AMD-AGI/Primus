051761a464919816e40284b64d0da530065c1d44
wenxie-amd <wen.xie@amd.com>
2025-10-14 01:34:58 -0500

    code clean

--- run_alloc.sh
+++
@@ -1,5 +0,0 @@
-salloc -N 8 \
-    --reservation=mi355-gpu-11_mi355-gpu-12_mi355-gpu-13_mi355-gpu-14_mi355-gpu-17_mi355-gpu-18_mi355-gpu-19_mi355-gpu-20_Reservation \
-    --ntasks-per-node=1 \
-    --cpus-per-task=96 \
-    --exclusive --mem=0
