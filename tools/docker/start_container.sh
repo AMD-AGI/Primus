ca7faf92e30b3436b96886df19ef0154059aff22
Sarthak Arora <sarthak.arora@amd.com>
2025-10-16 04:47:42 -0500

    fix typo

--- tools/docker/start_container.sh
+++ tools/docker/start_container.sh
@@ -6,7 +6,7 @@
 ###############################################################################
 
 PRIMUS_PATH=$(realpath "$(dirname "$0")/../..")
-DOCKER_IMAGE=${DOCKER_IMAGE:-" rocm/primus:v25.9_gfx950"}
+DOCKER_IMAGE=${DOCKER_IMAGE:-"rocm/primus:v25.9_gfx950"}
 DATA_PATH=${DATA_PATH:-"${PRIMUS_PATH}/data"}
 
 bash "${PRIMUS_PATH}"/tools/docker/docker_podman_proxy.sh run -d \
