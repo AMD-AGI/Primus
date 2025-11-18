6c53db7768ce40ca86fb5efe505c76538c31ad2f
wenxie-amd <wen.xie@amd.com>
2025-11-17 22:04:56 -0600

    update dpskv2, 3.2~3.2w tps

--- examples/moe_package/run_deepseek_v2_lite_pretrain_mi355x.sh
+++ examples/moe_package/run_deepseek_v2_lite_pretrain_mi355x.sh
@@ -7,7 +7,8 @@
 
 ######################### Training Docker and Variables #########################
 # export DOCKER_IMAGE=${DOCKER_IMAGE:="docker.io/rocm/pytorch-training-private:20250929_gfx950_25dot9_rc4"}
-export DOCKER_IMAGE="docker.io/tasimage/primus:pr-289"
+export DOCKER_IMAGE="docker.io/rocm/mad-private:primus_rocm7.1_ci_4096e28_20251114"
+# export DOCKER_IMAGE="docker.io/tasimage/primus:pr-289"
 export CLEAN_DOCKER_CONTAINER=1
 
 ######################### Training Environment Variables #########################
@@ -18,24 +19,25 @@ export GPU_MAX_HW_QUEUES=2
 # export GPU_MAX_HW_QUEUES=8
 
 # Set on Primus-Safe Platform
-export MASTER_ADDR=${MASTER_ADDR:-localhost}
-export MASTER_PORT=${MASTER_PORT:-1234}
-export NNODES=${PET_NNODES:-1}
-export NODE_RANK=${PET_NODE_RANK:-0}
-export GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+# export MASTER_ADDR=${MASTER_ADDR:-localhost}
+# export MASTER_PORT=${MASTER_PORT:-1234}
+# export NNODES=${PET_NNODES:-1}
+# export NODE_RANK=${PET_NODE_RANK:-0}
+# export GPUS_PER_NODE=${GPUS_PER_NODE:-8}
 
 # Set on AAC14 cluster
-# export NNODES=4
-# export USING_AINIC=1
-# export NCCL_IB_HCA="rocep105s0,rocep121s0,rocep137s0,rocep153s0,rocep233s0,rocep249s0,rocep25s0,rocep9s0"
-# export ANP_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/amd-anp-1.1.0-5"
-# export RCCL_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/rccl-drop-2025-08"
-# export NCCL_SOCKET_IFNAME="enp193s0f1np1"
-# export GLOO_SOCKET_IFNAME="enp193s0f1np1"
+export NNODES=1
+export USING_AINIC=1
+export NCCL_IB_HCA="rocep105s0,rocep121s0,rocep137s0,rocep153s0,rocep233s0,rocep249s0,rocep25s0,rocep9s0"
+export ANP_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/amd-anp-1.1.0-5"
+export RCCL_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/rccl-drop-2025-08"
+export NCCL_SOCKET_IFNAME="enp193s0f1np1"
+export GLOO_SOCKET_IFNAME="enp193s0f1np1"
 
 export HSA_NO_SCRATCH_RECLAIM=1
 export NVTE_CK_USES_BWD_V3=1
 # export USE_ROCM_AITER_ROPE_BACKEND=0
+export PRIMUS_TURBO_ATTN_V3_ATOMIC_FP32=0
 
 ######################### Training Config #########################
 MBS=12
@@ -65,13 +67,13 @@ TRAIN_ITERS=10
 # 1 - Turbo attention acceleration
 # 2 - Turbo grouped GEMM / MLP fusion
 # 3 - DeepEP acceleration
-# 4 - Sync-free MoE (stage 2)
+# 4 - Sync-free MoE (stage 1/2)
 # 5 - 1F1B MoE overlap
 # 6 - Zero-bubble pipeline optimizations
 # 7 - Arbitrary pipeline partition (8-way custom layout)
 # 8 - CPU NUMA binding helper
 # MoE_Features=(0 1 2 3 4 5 6 7 8)
-MoE_Features=(3 4)
+MoE_Features=(3 4 8)
 
 FEATURE_ARGS=()
 PRIMUS_TURBO_ENABLED="False"
@@ -258,6 +260,8 @@ echo "--------------------------------" | tee -a "$LOG_FILE"
 export SKIP_TRAIN=0
 
 	# --pp_warmup True \
+# bash ./examples/run_slurm_pretrain.sh \
+    # --multi_latent_attention True \
 bash ./examples/run_pretrain.sh \
     --micro_batch_size "$MBS" \
 	--global_batch_size "$GBS" \
