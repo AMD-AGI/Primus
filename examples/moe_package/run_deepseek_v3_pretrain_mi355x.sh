940bf8b92eef91c27d2d8b5e2d785ec17b36412f
wenxie-amd <wen.xie@amd.com>
2025-11-18 03:22:28 -0600

    update

--- examples/moe_package/run_deepseek_v3_pretrain_mi355x.sh
+++ examples/moe_package/run_deepseek_v3_pretrain_mi355x.sh
@@ -18,20 +18,20 @@ export GPU_MAX_HW_QUEUES=2
 # export GPU_MAX_HW_QUEUES=8
 
 # Set on Primus-Safe Platform
-export MASTER_ADDR=${MASTER_ADDR:-localhost}
-export MASTER_PORT=${MASTER_PORT:-1234}
-export NNODES=${PET_NNODES:-1}
-export NODE_RANK=${PET_NODE_RANK:-0}
-export GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+# export MASTER_ADDR=${MASTER_ADDR:-localhost}
+# export MASTER_PORT=${MASTER_PORT:-1234}
+# export NNODES=${PET_NNODES:-1}
+# export NODE_RANK=${PET_NODE_RANK:-0}
+# export GPUS_PER_NODE=${GPUS_PER_NODE:-8}
 
-# Set on AAC14 cluster
-# export NNODES=4
-# export USING_AINIC=1
-# export NCCL_IB_HCA="rocep105s0,rocep121s0,rocep137s0,rocep153s0,rocep233s0,rocep249s0,rocep25s0,rocep9s0"
-# export ANP_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/amd-anp-1.1.0-5"
-# export RCCL_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/rccl-drop-2025-08"
-# export NCCL_SOCKET_IFNAME="enp193s0f1np1"
-# export GLOO_SOCKET_IFNAME="enp193s0f1np1"
+Set on AAC14 cluster
+export NNODES=4
+export USING_AINIC=1
+export NCCL_IB_HCA="rocep105s0,rocep121s0,rocep137s0,rocep153s0,rocep233s0,rocep249s0,rocep25s0,rocep9s0"
+export ANP_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/amd-anp-1.1.0-5"
+export RCCL_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/rccl-drop-2025-08"
+export NCCL_SOCKET_IFNAME="enp193s0f1np1"
+export GLOO_SOCKET_IFNAME="enp193s0f1np1"
 
 export HSA_NO_SCRATCH_RECLAIM=1
 export NVTE_CK_USES_BWD_V3=1
@@ -45,7 +45,7 @@ TP=1
 ETP=1
 PP=2
 VPP=4
-EP=16
+EP=8
 CP=1
 CP_COMM_TYPE="a2a" # p2p, a2a, allgather or a2a+p2p
 ENABLE_MTP=False
@@ -71,7 +71,7 @@ TRAIN_ITERS=5
 # 7 - Arbitrary pipeline partition (8-way custom layout)
 # 8 - CPU NUMA binding helper
 # MoE_Features=(0 1 2 3 4 5 6 7 8)
-MoE_Features=(0)
+MoE_Features=(0 7)
 
 FEATURE_ARGS=()
 PRIMUS_TURBO_ENABLED="False"
@@ -172,7 +172,7 @@ for feature in "${MoE_Features[@]}"; do
 		# 32 stages for PP8VPP4
 		# FEATURE_ARGS+=("--pipeline_model_parallel_layout" "Et|(tt|)*30L")
 		# pp2 vpp4
-		# 1 + 6 + 1 stages
+		# 1 + 6 + 1 = 8 stages
 		# 1 + 2*6 = 13 layers
 		# FEATURE_ARGS+=("--pipeline_model_parallel_layout" 'Et|(tt|)*6L')
 		# FEATURE_ARGS+=("--pipeline_model_parallel_layout" 'Et|tt|tt|tt|tt|tt|tt|L')
@@ -241,7 +241,7 @@ mkdir -p "$LOG_DIR"
 rm -rf "$LOG_FILE"
 
 ######################### Training Job #########################
-export EXP="examples/megatron/configs/MI300X/deepseek_v3-pretrain.yaml"
+export EXP="examples/megatron/configs/MI355X/deepseek_v3-pretrain.yaml"
 
 echo "--------------------------------" | tee -a "$LOG_FILE"
 echo "Begin Training... $(date +%Y%m%d_%H%M%S)" | tee -a "$LOG_FILE"
@@ -261,9 +261,8 @@ export SKIP_TRAIN=0
     # --num_layers 16 \
     # --moe_layer_freq 1 \
 	# --pp_warmup True \
-# bash ./examples/run_slurm_pretrain.sh \
-bash ./examples/run_pretrain.sh \
---num_layers 13 \
+bash ./examples/run_slurm_pretrain.sh \
+--num_layers 16 \
 --moe_layer_freq 1 \
     --micro_batch_size "$MBS" \
 	--global_batch_size "$GBS" \
