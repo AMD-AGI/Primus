8e49af87d7de972ee5f1e3d84be217ea3aa9de17
wenxie-amd <wen.xie@amd.com>
2025-11-21 02:50:50 +0000

    improve pp-warmup efficiency

--- primus/modules/trainer/megatron/utils.py
+++ primus/modules/trainer/megatron/utils.py
@@ -262,16 +262,12 @@ def pp_warmup(args, config, model, optimizer):
             seq_len = args.seq_length // args.tensor_model_parallel_size // args.context_parallel_size
 
             for layer in model_chunk.module.module.decoder.layers:
-                attn_input = torch.randn(seq_len, 1, config.hidden_size, device="cuda", dtype=dtype)
+                dummy_input = torch.randn(seq_len, 1, config.hidden_size, device="cuda", dtype=dtype)
                 attention_mask = (
                     torch.tril(torch.ones((seq_len, seq_len), device="cuda")).unsqueeze(0).unsqueeze(0) == 0
                 )
-                attn_output = layer.self_attention(attn_input, attention_mask=attention_mask)
-                attn_output[0].backward(torch.ones_like(attn_output[0]))
-
-                mlp_input = torch.randn(seq_len, 1, config.hidden_size, device="cuda", dtype=dtype)
-                mlp_output = layer.mlp(mlp_input)
-                mlp_output[0].backward(torch.ones_like(mlp_output[0]))
+                dummy_output, _ = layer.forward(hidden_states=dummy_input, attention_mask=attention_mask)
+                dummy_output.backward(torch.ones_like(dummy_output))
 
             if model_chunk.use_forward_hook:
                 model_chunk.enable_forward_pre_hook()
