090756015750fb8880a192f98ec44a4f1d29da86
Xiaoming-AMD <xiaoming.peng@amd.com>
2025-12-02 08:02:15 +0000

    Add TrainerComponent base class and BaseTrainer patch integration tests

---
+++ primus/core/trainer/__init__.py
@@ -0,0 +1,13 @@
+###############################################################################
+# Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+#
+# See LICENSE for license information.
+###############################################################################
+
+from primus.core.trainer.base_trainer import BaseTrainer
+from primus.core.trainer.trainer_component import TrainerComponent
+
+__all__ = [
+    "TrainerComponent",
+    "BaseTrainer",
+]
