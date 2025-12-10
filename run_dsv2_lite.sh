bb94217b414bb67b42baf9956574a1c8e26765f5
Your Name <you@example.com>
2025-12-10 17:02:30 +0000

    update test script

--- run_dsv2_lite.sh
+++ run_dsv2_lite.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+# docker run -it --name dev_jax_rocm70 --ipc=host --network=host --device=/dev/kfd --device=/dev/dri --device=/dev/infiniband \
+# --cap-add=SYS_PTRACE --cap-add=CAP_SYS_ADMIN --security-opt seccomp=unconfined --group-add video --privileged \
+# -v ${HOME}:${HOME} "docker.io/rocm/primus:v25.9_gfx950" bash
+
+
 export HF_TOKEN=${HF_TOKEN:-"your_hf_token"}
 export USE_ROCM_AITER_ROPE_BACKEND=0
 export CLEAN_DOCKER_CONTAINER=0
 
-export USING_AINIC=1
-export NCCL_IB_HCA="rocep105s0,rocep121s0,rocep137s0,rocep153s0,rocep233s0,rocep249s0,rocep25s0,rocep9s0"
+export USING_AINIC=0
+# export NCCL_IB_HCA="rocep105s0,rocep121s0,rocep137s0,rocep153s0,rocep233s0,rocep249s0,rocep25s0,rocep9s0"
 # export AINIC_LIB="/apps/gpuperf/ainic-driver-20251007/lib/"
 export ANP_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/amd-anp-1.1.0-5"
 export RCCL_HOME_DIR="/shared/apps/ubuntu/rocm-7.0.1/rccl-drop-2025-08"
-export NCCL_SOCKET_IFNAME="enp193s0f1np1"
-export GLOO_SOCKET_IFNAME="enp193s0f1np1"
+# export NCCL_SOCKET_IFNAME="enp193s0f1np1"
+# export GLOO_SOCKET_IFNAME="enp193s0f1np1"
 
-export DOCKER_IMAGE="docker.io/rocm/pytorch-training-private:20250929_gfx950_25dot9_rc4"
+# export DOCKER_IMAGE="docker.io/rocm/pytorch-training-private:20250929_gfx950_25dot9_rc4"
 # export DOCKER_IMAGE="docker.io/rocm/7.0-preview:rocm7.0_preview_pytorch_training_mi35x_beta"
 # export DOCKER_IMAGE="docker.io/rocm/pyt-megatron-lm-jax-nightly-private:pytorch_gfx950_20250819"
 export CPUS_PER_TASK=128
@@ -64,7 +69,7 @@ LOG_FILE=$LOG_DIR/training.log
 echo $LOG_FILE
 
 EXPORT_CONFIG=$LOG_DIR/config.yaml
-bash ./examples/run_local_pretrain.sh --micro_batch_size $MBS \
+bash ./examples/run_pretrain.sh --micro_batch_size $MBS \
                                       --global_batch_size $GBS \
                                       --tensor_model_parallel_size $TP \
                                       --expert_tensor_parallel_size $ETP \
@@ -80,7 +85,7 @@ bash ./examples/run_local_pretrain.sh --micro_batch_size $MBS \
                                       ${VPP_CONFIG} \
                                       ${FP8_CONFIG} \
                                       --profile True \
-                                      --disable_profiler_activity_cpu True \
+                                      --disable_profiler_activity_cpu False \
                                       --use_pytorch_profiler True \
                                       --profile_step_start 5 \
                                       --profile_step_end 6 \
