277f3e1c5276187d6edec460e5197873adf1a67c
Mingyu Yang <Mingyu.Yang@amd.com>
2026-01-13 19:07:59 +0000

    add zebra-llama (hybrid mla mamba model) support

---
+++ primus/backends/megatron/core/models/hybrid/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+#
+# See LICENSE for license information.
+###############################################################################
+
+"""Hybrid Mamba+MLA layer specifications for Megatron-LM."""
+
+from .hybrid_mamba_mla_layer_specs import hybrid_inference_stack_spec, hybrid_stack_spec
+
+__all__ = ["hybrid_stack_spec", "hybrid_inference_stack_spec"]
+
