718d03661a93154b369ae39b18e6a2bf8aec6583
zhenhuang12 <Zhen.Huang@amd.com>
2026-01-14 01:35:23 +0000

    update build_uccl

--- runner/helpers/hooks/04_build_uccl.sh
+++ runner/helpers/hooks/04_build_uccl.sh
@@ -5,7 +5,7 @@
 # See LICENSE for license information.
 ###############################################################################
 #
-# System hook: enable build uccl settings..
+# System hook: enable build uccl settings.
 #
 # Trigger:
 #   export USING_UCCL=1
@@ -18,36 +18,36 @@ if [[ "${USING_UCCL:-0}" != "1" ]]; then
     exit 0
 fi
 
+UCCL_DIR="/tmp/uccl"
+
 LOG_INFO_RANK0 "[hook system] USING_UCCL=1 → Building uccl in /tmp "
 
-pushd /tmp
 
-if [ -d uccl ]; then
-    LOG_INFO_RANK0 "[hook system] Found existed uccl in /tmp"
+if [ -d "$UCCL_DIR" ]; then
+	LOG_INFO_RANK0 "[hook system] Found existed uccl in /tmp"
 else
-	git clone https://github.com/uccl-project/uccl.git
+	cd /tmp && git clone https://github.com/uccl-project/uccl.git
 fi
 
-cd uccl
-pushd ep
+pushd $UCCL_DIR
 
 # install dependencies
 apt update && apt install -y rdma-core libibverbs-dev libnuma-dev libgoogle-glog-dev
 
 LOG_INFO_RANK0 "[hook system] Building uccl ep"
-python3 setup.py build
+cd ep && python3 setup.py build && cd ..
 
 LOG_INFO_RANK0 "[hook system] Building uccl ep done"
-popd
+
 cp ep/build/**/*.so uccl
 
 python3 setup.py install
 LOG_INFO_RANK0 "[hook system] Install uccl done"
 # install deep_ep_wrapper
-pushd ep/deep_ep_wrapper
+cd $UCCL_DIR/ep/deep_ep_wrapper
 python3 setup.py install
 LOG_INFO_RANK0 "[hook system] Install deep_ep done"
-popd
-popd
 
 LOG_INFO_RANK0 "[hook system] Building uccl done."
+
+popd
