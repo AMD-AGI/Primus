c43e69dc74d5dc936232dff564577f17e1da669b
Anshu Raina <Anshu.Raina@amd.com>
2026-01-27 18:42:51 -0800

    Fix code formatting: apply black formatting with line-length=110
    - Fix import spacing (add blank lines after imports)
    - Fix string quotes (single to double quotes)
    - Fix trailing whitespace
    - Fix function spacing (add blank lines between functions)
    - Format all affected files to pass CI black check

--- primus/core/projection/module_profilers/dense_mlp.py
+++ primus/core/projection/module_profilers/dense_mlp.py
@@ -1,4 +1,5 @@
 from typing import Optional, Tuple
+
 ###############################################################################
 # Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
 #
