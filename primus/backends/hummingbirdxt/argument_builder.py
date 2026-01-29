4b470d4f715ece238c4527ab0b1474cbda314126
LI MOU <lxglbk@gmail.com>
2026-01-29 07:10:30 +0000

    update hummingbirdxt

--- primus/backends/hummingbirdxt/argument_builder.py
+++ primus/backends/hummingbirdxt/argument_builder.py
@@ -6,6 +6,7 @@
 
 from __future__ import annotations
 
+from datetime import datetime
 from types import SimpleNamespace
 from typing import Any, Dict
 
@@ -16,15 +17,31 @@ from primus.core.utils.yaml_utils import (
 )
 
 
+def _load_hummingbirdxt_default_args() -> Dict[str, Any]:
+    from train import get_arg_parser
+
+    parser = get_arg_parser()
+
+    default_args_dict = {}
+    for action in parser._actions:
+        if not action.required and action.dest != "help":
+            default_args_dict[action.dest] = action.default
+
+    # used for wandb, will be overwritten if it has been set in yaml config
+    default_args_dict["config_name"] = "primus_train_hummingbirdxt_{}".format(
+        datetime.now().strftime("%Y%m%d%H%M%S")
+    )
+    return default_args_dict
+
+
 class HummingbirdXTArgBuilder:
 
     def __init__(self) -> None:
-        self.config: Dict[str, Any] = {}
+        self.config: Dict[str, Any] = _load_hummingbirdxt_default_args()
 
     def update(self, values: SimpleNamespace) -> "HummingbirdXTArgBuilder":
         # Convert SimpleNamespace to dict
         values_dict = nested_namespace_to_dict(values)
-
         # Directly merge into the working configuration
         self.config = deep_merge(self.config, values_dict)
         return self
