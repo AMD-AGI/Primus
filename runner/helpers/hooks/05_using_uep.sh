144a1032ec57e0cd2480122e90dfe3c6c5efdd0d
zhenhuang12 <Zhen.Huang@amd.com>
2026-02-28 07:26:33 +0000

    nits

--- runner/helpers/hooks/05_using_uep.sh
+++ runner/helpers/hooks/05_using_uep.sh
@@ -14,21 +14,21 @@
 
 
 if [ "$USING_UEP" == "1" ]; then
-    LOG_INFO "USING_UEP is enabled, checking required packages..."
+    LOG_INFO_RANK0 "USING_UEP is enabled, checking required packages..."
 
     if ! python3 -m pip show uccl &>/dev/null || ! python3 -m pip show deep_ep &>/dev/null; then
         LOG_ERROR "uccl is not installed! Please use pre-installed primus image or set REBUILD_UCCL=1."
         exit 1
     fi
-    LOG_INFO "uccl package is installed: $(python3 -m pip show uccl | grep Version)"
-    LOG_INFO "deep_ep package is installed: $(python3 -m pip show deep_ep | grep Version)"
+    LOG_INFO_RANK0 "uccl package is installed: $(python3 -m pip show uccl | grep Version)"
+    LOG_INFO_RANK0 "deep_ep package is installed: $(python3 -m pip show deep_ep | grep Version)"
 
     if [ "$ENABLE_NUMA_BINDING" != "1" ]; then
         LOG_WARN "ENABLE_NUMA_BINDING is not enabled! Please set ENABLE_NUMA_BINDING=1 to avoid dataloader worker exited unexpectedly."
     fi
 
     export PRIMUS_TURBO_MOE_DISPATCH_COMBINE_BACKEND=DEEP_EP
-    LOG_INFO "PRIMUS_TURBO_MOE_DISPATCH_COMBINE_BACKEND set to DEEP_EP"
+    LOG_INFO_RANK0 "PRIMUS_TURBO_MOE_DISPATCH_COMBINE_BACKEND set to DEEP_EP"
 
     # network settings for UCCL
     export UCCL_IB_GID_INDEX=${UCCL_IB_GID_INDEX:-$NCCL_IB_GID_INDEX}
@@ -48,15 +48,15 @@ if [ "$USING_UEP" == "1" ]; then
     fi
 
 
-    LOG_INFO "==========UCCL Network Settings=========="
-    LOG_INFO "UCCL_IB_GID_INDEX: $UCCL_IB_GID_INDEX"
-    LOG_INFO "UCCL_IB_HCA: $UCCL_IB_HCA"
-    LOG_INFO "UCCL_SOCKET_IFNAME: $UCCL_SOCKET_IFNAME"
-    LOG_INFO "UCCL_IB_MAX_INFLIGHT_NORMAL: $UCCL_IB_MAX_INFLIGHT_NORMAL"
-    LOG_INFO "UCCL_IB_MAX_INFLIGHT_LOW_LATENCY: $UCCL_IB_MAX_INFLIGHT_LOW_LATENCY"
-    LOG_INFO "UCCL_IB_MAX_INFLIGHT_BYTES: $UCCL_IB_MAX_INFLIGHT_BYTES"
-    LOG_INFO ""
+    LOG_INFO_RANK0 "==========UCCL Network Settings=========="
+    LOG_INFO_RANK0 "UCCL_IB_GID_INDEX: $UCCL_IB_GID_INDEX"
+    LOG_INFO_RANK0 "UCCL_IB_HCA: $UCCL_IB_HCA"
+    LOG_INFO_RANK0 "UCCL_SOCKET_IFNAME: $UCCL_SOCKET_IFNAME"
+    LOG_INFO_RANK0 "UCCL_IB_MAX_INFLIGHT_NORMAL: $UCCL_IB_MAX_INFLIGHT_NORMAL"
+    LOG_INFO_RANK0 "UCCL_IB_MAX_INFLIGHT_LOW_LATENCY: $UCCL_IB_MAX_INFLIGHT_LOW_LATENCY"
+    LOG_INFO_RANK0 "UCCL_IB_MAX_INFLIGHT_BYTES: $UCCL_IB_MAX_INFLIGHT_BYTES"
+    LOG_INFO_RANK0 ""
 else
     export PRIMUS_TURBO_MOE_DISPATCH_COMBINE_BACKEND=TURBO
-    LOG_INFO "USING_UEP is disabled. PRIMUS_TURBO_MOE_DISPATCH_COMBINE_BACKEND set to TURBO"
+    LOG_INFO_RANK0 "USING_UEP is disabled. PRIMUS_TURBO_MOE_DISPATCH_COMBINE_BACKEND set to TURBO"
 fi
