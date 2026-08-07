e775157cd707dd48a50cd73fb23bf5c4c82b162b
kailash <kailash.gogineni@amd.com>
2026-08-07 16:44:39 +0000

    Revert optional megatron.mlperf __init__ and log suppression churn

--- primus/mlperf_log_suppression.py
+++ primus/mlperf_log_suppression.py
@@ -73,7 +73,7 @@ QUIET_LOGGER_NAMES = (
     # through loguru instead, which we drop at the FD level below).
     "primus",
     "primus_turbo",
-    "primus.backends.megatron_bridge.mlperf_sft",
+    "primus_mllog",
     # TransformerEngine import / RoPE banners that use stdlib logging.
     "transformer_engine",
     # torch.distributed elastic / launcher chatter.
