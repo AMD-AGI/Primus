11c9ebe0dc6044c3bb565b4f1c4e9a406560a7c5
Han Wang <Han.Wang@amd.com>
2026-09-09 10:23:11 +0800

    fix: typo

--- primus/backends/megatron/core/extensions/primus_turbo.py
+++ primus/backends/megatron/core/extensions/primus_turbo.py
@@ -1361,7 +1361,7 @@ class PrimusTurboRowParallelLinear(TERowParallelLinear):
                 if get_num_microbatches() == 1:
                     if is_first_microbatch:
                         self.quantized_weight_buffer = torch.empty(
-                            0, device=weights.device, dtype=float4_e2m1fn_x2
+                            0, device=weight.device, dtype=float4_e2m1fn_x2
                         )
                     out = primus_turbo_torch.ops.gemm_fp4(
                         x,
@@ -1567,7 +1567,7 @@ class PrimusTurboColumnParallelLinear(TEColumnParallelLinear):
                 if get_num_microbatches() == 1:
                     if is_first_microbatch:
                         self.quantized_weight_buffer = torch.empty(
-                            0, device=weights.device, dtype=float4_e2m1fn_x2
+                            0, device=weight.device, dtype=float4_e2m1fn_x2
                         )
                     out = primus_turbo_torch.ops.gemm_fp4(
                         x,
@@ -1786,7 +1786,7 @@ class PrimusTurboLayerNormColumnParallelLinear(TELayerNormColumnParallelLinear):
                 if get_num_microbatches() == 1:
                     if is_first_microbatch:
                         self.quantized_weight_buffer = torch.empty(
-                            0, device=weights.device, dtype=float4_e2m1fn_x2
+                            0, device=weight.device, dtype=float4_e2m1fn_x2
                         )
                     out = primus_turbo_torch.ops.gemm_fp4(
                         inp,
